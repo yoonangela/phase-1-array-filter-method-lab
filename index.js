08462f778480fc4f27b5171bcd1e6eb25d2412d8
Yeji-Yoon <angelayoon0102@gmail.com>
2025-01-31 00:13:26 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(array, string){
+    
+    return array.filter((value)=>value.toUpperCase()===string.toUpperCase())
+}
+
+function fuzzyMatch(array, string){
+    return array.filter((value)=> value.startsWith(string))
+}
+
+function matchName(arrayOfo, string){
+    return arrayOfo.filter((object)=> object["name"]===string)
+}
+
